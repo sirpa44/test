c666cd9b2c3805ffd475aab46969e7dc77654c2d
sirpa44 <sirpa.antoine@gmail.com>
2018-11-22 12:07:16 +0100

    more clean

--- Src/App/Router.php
+++ Src/App/Router.php
@@ -1,31 +1,29 @@
 <?php
-namespace Oat\App {
+namespace Oat\App;
 
+class Router
+{
+    private $interfaceControllerPath = '\Oat\Controller\ControllerInterface';
 
-    class Router
+    /**
+     * lead to the controller
+     * @param $parameters
+     * @return array
+     * @throws \Exception
+     */
+    public function route($parameters)
     {
-        private $interfaceControllerPath = '\Oat\Controller\ControllerInterface';
-
-        /**
-         * lead to the controller
-         * @param $parameters
-         * @return array
-         * @throws \Exception
-         */
-        public function route($parameters)
-        {
-            if (!isset($parameters['controller']) || !isset($parameters['method'])) {
-                throw new \Exception("method or controller invalid");
-            }
-            $controllerClassName = ucfirst($parameters['controller']) . 'Controller';
-            $controllerName = '\Oat\Controller\\' . $controllerClassName;
-            if (!is_a($controllerName, $this->interfaceControllerPath, true)) {
-                throw new \Exception("controller invalid");
-            }
-            $method = $parameters['method'];
-            $controller = new $controllerName();
-            unset($parameters['method'], $parameters['controller']);
-            return $controller->action($parameters, $method);
+        if (!isset($parameters['controller']) || !isset($parameters['method'])) {
+            throw new \Exception("method or controller invalid");
         }
+        $controllerClassName = ucfirst($parameters['controller']) . 'Controller';
+        $controllerName = '\Oat\Controller\\' . $controllerClassName;
+        if (!is_a($controllerName, $this->interfaceControllerPath, true)) {
+            throw new \Exception("controller invalid");
+        }
+        $method = $parameters['method'];
+        $controller = new $controllerName();
+        unset($parameters['method'], $parameters['controller']);
+        return $controller->action($parameters, $method);
     }
 }
